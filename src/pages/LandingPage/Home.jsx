1fed136e9ff01f9758d53440f135d8b268fea132
gabbiee33 <gabriellepereirarocha96@gmail.com>
2024-10-15 11:57:45 -0300

    componente rodape, gradiente rodape

--- src/pages/LandingPage/Home.jsx
+++ src/pages/LandingPage/Home.jsx
@@ -1,6 +1,7 @@
 import './Home.scss';
 import { Link } from 'react-router-dom';
 import Cabecalho from '../../components/Header/cabecalho.jsx';
+import Rodape from '../../components/Body/rodape.jsx';
 
 export default function Landingpage() {
   return (
@@ -67,36 +68,11 @@ export default function Landingpage() {
 
       </div>
 
-      <div className='rodape'>
-
-        <div className='emp'>
-
-          <div className='text'>
-            <h1>Empresa</h1>
-            <p>Somos uma empresa em formação que visa o crescimento de todas pessoas através de belos trabalhos feitos em resina. Há, mais ou menos, quatro anos que trabalhamos com resina.</p>
-          </div>
-
-        </div>
-
-        <div className='cont'>
-
-          <div className='text'>
-            <h1>Entre em Contato</h1>
-            <p>+55 11 95927-xxxx <br />
-              +82 98898-xxxx <br />
-              contato@resinarte.com <br />
-              São Paulo - São Paulo</p>
-          </div>
-
-        </div>
-
-        <div className='icones'>
-          <i className="fa-brands fa-instagram"></i>
-          <i className="fa-brands fa-square-facebook"></i>
-          <i className="fa-brands fa-youtube"></i>
-        </div>
+      <div className='gradientee'>
 
       </div>
+
+      <Rodape />
 
     </div>
 
