2a028b570a860c7c6be01c3241f2d626c071d30a
FFefo <felpo.correiacosta@gmail.com>
2024-10-18 18:23:01 -0300

    admin iniciada

--- src/pages/admin/admin.jsx
+++ src/pages/admin/admin.jsx
@@ -1,2 +1,29 @@
 import './admin.scss'
+import Cabecalho from '../../components/Header/cabecalho.jsx'
 
+export default function LandingPageAdmin() {
+    return (
+        <div className='pagina-admin'>
+
+            <Cabecalho />
+
+            <div className='ferramentas'>
+
+                    <div className='text'>
+                        <h1>Bem vinda, Dani.</h1>
+                        <p>Aqui você pode controlar o conteúdo de seu site, além de usufruir de outras ferramentas.</p>
+                    </div>
+
+                    <div className='icon'>
+                        <i className="fa-solid fa-bars"></i>
+                    </div>
+
+                <div className='op'>
+
+                </div>
+
+            </div>
+
+        </div>
+    )
+}
--- src/router.jsx
+++ src/router.jsx
@@ -2,6 +2,7 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Landingpage from "./pages/LandingPage/Home";
 import Vitrine from "./pages/Vitrine/catalogo.jsx";
 import Login from "./pages/login/login";
+import LandingPageAdmin from "./pages/admin/admin.jsx";
 
 export function Navegacao() {
     return (
@@ -10,6 +11,7 @@ export function Navegacao() {
                 <Route path="/" element={<Landingpage />} />
                 <Route path="/login" element={<Login />} />
                 <Route path="/vitrine" element={<Vitrine />} />
+                <Route path="/admin" element={<LandingPageAdmin />} />
             </Routes>
         </BrowserRouter>
     )
